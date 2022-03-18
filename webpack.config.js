a412c3e8ffe6c716bded4e9e5fbb339274e6bba6
Georg Guba <georg.guba@gmail.com>
2022-03-18 22:09:37 +0100

    new repository structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.ts',
+    entry: './index.ts',
     devtool: 'inline-source-map',
     devServer: {
         static: ['.'],
