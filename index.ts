b0dd9e417f8a3b2e46fb323d8ce4e023510a1b3e
devium <devium@users.noreply.github.com>
2022-03-18 21:10:43 +0000

    Deploying to gh-pages from master @ a412c3e8ffe6c716bded4e9e5fbb339274e6bba6 🚀

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+/// <reference path="node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
